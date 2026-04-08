56a1afca24eb12b8004b08cca5fb882e5c243c63
Nayan D'Souza <66888425+nayancat11@users.noreply.github.com>
2026-04-08 14:06:51 -0400

    Add parameterized quantum circuit with optimization

--- ParametrizedQuantumCircuits.py
+++ ParametrizedQuantumCircuits.py
@@ -1,1 +1,13 @@
+from qiskit.transpiler import generate_preset_pass_manager
+from qiskit.circuit import Parameter
 
+angle = Parameter("angle")  # undefined number
+
+# Create and optimize circuit once
+qc = QuantumCircuit(1)
+qc.rx(angle, 0)
+qc = generate_preset_pass_manager(
+    optimization_level=3, basis_gates=["u", "cx"]
+).run(qc)
+
+qc.draw("mpl")
